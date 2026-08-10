61dcd24970f2235c0887a89248aaf4e4b38d8fd6
Reinaldo Mock <nhd.schoolren@gmail.com>
2026-08-10 14:18:14 -0400

    narrow public package API

--- src/reylog/__init__.py
+++ src/reylog/__init__.py
@@ -1,6 +1,6 @@
 """reylog: a small, opinionated logging facade backed by Loguru."""
 
-from .logger import ReyLogger, logger
+from .logger import logger
 
-__all__ = ["ReyLogger", "logger"]
+__all__ = ["logger"]
 __version__ = "0.1.0"
